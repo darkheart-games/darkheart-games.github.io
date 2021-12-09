b2da16662afcd68379e21e3eaed30fee8a63091a
Robin Siegl <92427486+darkheart-games@users.noreply.github.com>
2021-12-09 13:55:00 +0100

    Delete main.js

--- _assets/main.js
+++
@@ -1,9 +0,0 @@
-const btnShowLibrary = document.getElementById("show-game-library");
-const contentMain = document.getElementById("main");
-
-btnShowLibrary.addEventListener("click", () => {
-  contentMain.classList.add("show");
-  setTimeout(() => {
-    contentMain.classList.add("content");
-  }, 1000);
-});
